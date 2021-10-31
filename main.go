87306d2316e7b8790a433805320ab7792c2015e3
Haardik Dharma <dharmahaardik08@gmail.com>
2021-11-01 00:24:10 +0530

    Revert "Make namespace configurable"
    This reverts commit 78a16798bce93649fe1f46f5e03f91237cc76f2e.

--- main.go
+++ main.go
@@ -39,7 +39,6 @@ func main() {
 	StopChan := make(chan struct{})
 
 	gRPCPtr := flag.String("gRPC", "", "gRPC server information")
-	namespacePtr := flag.String("namespace", "", "namespace of the cluster")
 
 	flag.Parse()
 	gRPC := ""
@@ -52,14 +51,6 @@ func main() {
 		} else {
 			gRPC = "localhost:32767"
 		}
-	}
-
-	nameSpace := ""
-
-	if *namespacePtr != "" {
-		nameSpace = *namespacePtr
-	} else {
-		nameSpace = "default"
 	}
 
 	conn, err := grpc.Dial(gRPC, grpc.WithInsecure())
@@ -98,9 +89,9 @@ func main() {
 						os.Exit(-1)
 					}
 
-					//fmt.Printf("Created policy report!")
+					fmt.Printf("Created policy report!")
 
-					r, err = report.Write(r, nameSpace, filepath.Join(homedir.HomeDir(), ".kube", "config"))
+					r, err = report.Write(r, "multiubuntu", filepath.Join(homedir.HomeDir(), ".kube", "config"))
 					if err != nil {
 						fmt.Printf("failed to create policy reports: %v \n", err)
 						os.Exit(-1)
